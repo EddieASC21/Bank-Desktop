86055fabad0b181f9ca421e09c3d85842c7ac21f
Eddie Ramirez <87774596+EddieASC21@users.noreply.github.com>
2025-06-09 19:05:58 -0400

    added styling to the home page

--- frontend/Bank-Desktop/src/Home.jsx
+++ frontend/Bank-Desktop/src/Home.jsx
@@ -1,15 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import './styles/Home.css';
 
-function Home(){
+function Home() {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-      navigate("/"); 
+        navigate("/");
     };
-    return(
+
+    return (
         <div>
-            <h1>Welcome Back!</h1>
-            <button onClick={handleLogout}>Logout</button>
+            <header className="home-header">
+                <h1>Welcome Back!</h1>
+                <button onClick={handleLogout}>Logout</button>
+            </header>
+
             <nav>
                 <ul>
                     <li>Zelle</li>
@@ -17,26 +22,29 @@ function Home(){
                     <li>Deposit</li>
                 </ul>
             </nav>
+
             <h3>Account Balances</h3>
             <section>
                 <div>
                     <h4>Checking's Account</h4>
-                    <p>$3444.00</p>
+                    <p>$3,444.00</p>
                 </div>
             </section>
+
             <section>
                 <div>
                     <h4>Saving's Account</h4>
                     <p>$20.00</p>
                 </div>
             </section>
+
             <section>
                 <div>
                     <h4>Recent Transactions</h4>
                     <ul>
-                        <li>Starbucks: -8.75</li>
-                        <li>Chipotle: -12.74</li>
-                        <li>Zelle: +10.00</li>
+                        <li>Starbucks: -$8.75</li>
+                        <li>Chipotle: -$12.74</li>
+                        <li>Zelle: +$10.00</li>
                     </ul>
                 </div>
             </section>
@@ -44,4 +52,4 @@ function Home(){
     );
 }
 
-export default Home
+export default Home;
